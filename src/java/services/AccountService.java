757592c1d154e95fc7c5672276505a3c932193fd
dorge <dorge@DESKTOP-5EQI8D0>
2020-12-15 00:50:03 -0700

    Done

--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -4,6 +4,17 @@ import dataaccess.UserDB;
 import models.User;
 
 public class AccountService {
+    public User getEmail(String email){
+                    UserDB userDB = new UserDB();
+        try{
+              User user = userDB.get(email);
+              return user;
+        }
+        catch(Exception ex){
+            
+        }
+           return null;  
+    }
     
     public User login(String email, String password) {
         UserDB userDB = new UserDB();
